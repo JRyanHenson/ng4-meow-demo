935f89a5abcc1ca1fe67b758c50104a80e2bef02
JRyanHenson <hensojr@gmail.com>
2017-08-24 10:33:05 -0600

    Fixed html issue.

--- public_html/templates/home.php
+++ public_html/templates/home.php
@@ -1,18 +1,17 @@
 <div class="sfooter">
 	<div class="sfooter-content">
-
-	<header...>
-	<main class="bg p-t-nav">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-8 col-md-offset-4">
-					<div class="jumbotron text-right">
-						<h1>Welcome to the Meow App!</h1>
-						<p class="lead">Now with 100% more fuzzy! =^. .^=</p>
+		<main class="bg p-t-nav">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-4">
+						<div class="jumbotron text-right">
+							<h1>Welcome to the Meow App!</h1>
+							<p class="lead">Now with 100% more fuzzy! =^. .^=</p>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</main>
-</div><!--/.sfooter-content-->
+		</main>
+	</div><!--/.sfooter-content-->
 </div>
+
